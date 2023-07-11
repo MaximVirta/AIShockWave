aeb79e0b9726817dd696afeaade975f85cd4c8fe
admin <pyjorunk@student.jyu.fi>
2023-07-11 14:59:27 +0200

    Added axis labels and fixed spacing.

--- hydro/plot-images.py
+++ hydro/plot-images.py
@@ -10,10 +10,13 @@ for i in range(3):
     for iax, ax in enumerate(axes):
         im = ax.imshow(images[j][0], interpolation = 'none') #extent = [-0.8, 0.8, 0, 2*np.pi], 
         plt.colorbar(im, ax=ax)
+        ax.set_xlabel('eta')
         ax.set_xticks([0,7,15,23,31], np.round(np.linspace(-0.8, 0.8, 5), 2))
+        ax.set_ylabel('phi')
         ax.set_yticks([0,7,15,23,31], np.round(np.linspace(-np.pi, np.pi, 5), 2))
         j +=1
 		#ax.set(title='{} jet: $p_T=${:.0f} GeV'.format(['QCD','top'][iax], [jetpep0,jetpep][iax][idx][0][0]))
+    plt.subplots_adjust(wspace=0.5)
     fig.suptitle("Particle images")
     axes[0].set_title("sample {}".format(j-2))
     axes[1].set_title("sample {}".format(j-1))
