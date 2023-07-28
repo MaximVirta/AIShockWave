ea4daf73ac7454afdc30d42b3cdb51cf89e3433b
admin <pyjorunk@student.jyu.fi>
2023-07-28 15:40:59 +0200

    loop progress printing

--- hydro/has_nan.py
+++ hydro/has_nan.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import sys
 
 def has_nan(array):
     '''
@@ -14,13 +15,18 @@ def main():
     howmanyNaNs = 0
     file_list = os.listdir(directory)
     n_files = len(file_list)
+    i = 0
     for filename in file_list:
+        i += 1
         filepath = os.path.join(directory, filename)
         compressed = np.load(filepath, allow_pickle=False)
         flowdata = compressed['flow_data']
         if has_nan(flowdata):
             howmanyNaNs += 1
-        print('files containing NaNs: ', howmanyNaNs, ' /', n_files)
+        if(i%100 == 0):
+            sys.stdout.write("\r{0}".format((float(i)/n)*100))
+            sys.stdout.flush()
+    print('files containing NaNs: ', howmanyNaNs, ' /', n_files)
     
 if __name__ == '__main__':
     main()
