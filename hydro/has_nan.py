079375ba96cd4976efe4069449dadec21662144e
admin <pyjorunk@student.jyu.fi>
2023-07-31 16:36:54 +0200

    now works with new directory substructure

--- hydro/has_nan.py
+++ hydro/has_nan.py
@@ -1,7 +1,3 @@
-import numpy as np
-import os
-import sys
-
 def has_nan(array):
     '''
     checks if given array has a NaN inside
@@ -10,12 +6,14 @@ def has_nan(array):
     return np.isnan(array).any()
 
 
-def main():
+def main(batchn):
     directory = '/scratch/project_2003154/MachineLearning/FirstImages5TeV/'
+    directory += batchn + '/'
     howmanyNaNs = 0
     file_list = os.listdir(directory)
     n_files = len(file_list)
     i = 0
+    start = time.time()
     for filename in file_list:
         filepath = os.path.join(directory, filename)
         if not os.path.isfile(filepath): continue
@@ -24,10 +22,16 @@ def main():
         flowdata = compressed['flow_data']
         if has_nan(flowdata):
             howmanyNaNs += 1
-        if(i%100 == 0):
-            sys.stdout.write("\r{0}".format((float(i)/n_files)*100))
-            sys.stdout.flush()
-            print('files containing NaNs: ', howmanyNaNs, ' /', i)
+        #if(i%100 == 0):
+           # sys.stdout.write("\r{0}".format((float(i)/n_files)*100))
+           # sys.stdout.flush()
+           # print('files containing NaNs: ', howmanyNaNs, ' /', i)
+    #print('checked', i, ' / ', n_files)
+    print('files containing NaNs: ', howmanyNaNs, ' /', i)
+    end = time.time()
+    #print(end-start)
+
 
 if __name__ == '__main__':
-    main()
+    batchn = sys.argv[1]
+    main(batchn)
